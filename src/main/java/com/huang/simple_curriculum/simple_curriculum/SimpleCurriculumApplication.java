27cd0ae36f3944a973912d70626530592531f2ae
Createsequence <841396397@qq.com>
2020-02-23 11:05:26 +0800

    提交初始项目

---
+++ src/main/java/com/huang/simple_curriculum/simple_curriculum/SimpleCurriculumApplication.java
@@ -0,0 +1,13 @@
+package com.huang.simple_curriculum.simple_curriculum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleCurriculumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleCurriculumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/huang/simple_curriculum/simple_curriculum/SimpleCurriculumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huang.simple_curriculum.simple_curriculum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleCurriculumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
